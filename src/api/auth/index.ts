c5f847301e0cc2c734a783b33789b811d5bfc03c
dipzera <nathanielthomaz@gmail.com>
2021-02-15 10:05:50 +0200

    split api functionality in to new folders

---
+++ src/api/auth/index.ts
@@ -0,0 +1,55 @@
+import HttpService from "../";
+import { API_AUTH_URL } from "../../configs/AppConfig";
+import { IAccount } from "../../redux/reducers/Account";
+import { ApiDecorator, ApiResponse } from "../types";
+const publicIp = require("react-public-ip");
+export class AuthService extends HttpService {
+  public constructor() {
+    super(`${API_AUTH_URL}`);
+  }
+
+  public Login = async (Email: string, Password: string) =>
+    this.instance.post<ApiDecorator<ApiResponse, "Token", string>>(
+      "/AuthorizeUser",
+      {
+        Email,
+        Password,
+        info: (await publicIp.v4()) || ("" as string),
+      }
+    );
+
+  public SendActivationCode = async (UserID?: number) =>
+    this.instance.get<ApiResponse>("/SendActivationCode", {
+      params: { UserID },
+    });
+
+  public ResetPassword = async (Email: string) =>
+    this.instance.post<ApiResponse>("/ResetPassword", {
+      Email,
+      info: (await publicIp.v4()) || "",
+    });
+
+  public RegisterUser = async (data: IAccount) =>
+    this.instance.post<ApiResponse>("/RegisterUser", {
+      ...data,
+    });
+
+  public GetManagedToken = async (CompanyID: number) =>
+    this.instance.get<ApiDecorator<ApiResponse, "Token", string>>(
+      "/GetManagedToken",
+      {
+        params: { CompanyID },
+      }
+    );
+
+  public ChangePassword = async (NewPassword: string, OldPasword: string) =>
+    this.instance.post<ApiResponse>("/ChangePassword", {
+      NewPassword,
+      OldPasword,
+    });
+
+  public ActivateUser = async (Token: string) =>
+    this.instance.get<ApiResponse>("/ActivateUser", {
+      params: { Token },
+    });
+}
