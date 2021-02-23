9a557aaac762dda30d73bc8c3ee3a5e6f0cb97bf
dipzera <nathanielthomaz@gmail.com>
2021-02-23 17:59:59 +0200

    removed useless switch and route inside manage component

--- src/views/app-views/manage/index.tsx
+++ src/views/app-views/manage/index.tsx
@@ -14,44 +14,40 @@ const Manage = (props: any) => {
   return (
     <>
       <Switch>
-        <Route path={props.match.ur}>
-          <Switch>
-            <Route exact path={props.match.url}>
-              <h4 className="text-center mb-3">Choose cloud</h4>
-              <div className="text-center mb-5">
-                <Button className="mr-3">
-                  <Link
-                    to={`${props.match.url}/instance?cloud=${EnDbServerLocation.PUBLIC}`}
-                  >
-                    Public
-                  </Link>
-                </Button>
-                <Button>
-                  <Link
-                    to={`${props.match.url}/instance?cloud=${EnDbServerLocation.PRIVATE}`}
-                  >
-                    Private
-                  </Link>
-                </Button>
-              </div>
-              <div className="text-center">
-                <img
-                  width={600}
-                  src={process.env.PUBLIC_URL + "/img/others/img-18.png"}
-                  alt="Cloud"
-                />
-              </div>
-            </Route>
-            <Route exact path={`${props.match.url}/instance`}>
-              <Instance {...props} />
-            </Route>
-            <Route path={`${props.match.url}/instance/create`}>
-              <InstanceCreate {...props} />
-            </Route>
-            <Route path={`${props.match.url}/instance/edit`}>
-              <InstanceEdit {...props} />
-            </Route>
-          </Switch>
+        <Route exact path={props.match.url}>
+          <h4 className="text-center mb-3">Choose cloud</h4>
+          <div className="text-center mb-5">
+            <Button className="mr-3">
+              <Link
+                to={`${props.match.url}/instance?cloud=${EnDbServerLocation.PUBLIC}`}
+              >
+                Public
+              </Link>
+            </Button>
+            <Button>
+              <Link
+                to={`${props.match.url}/instance?cloud=${EnDbServerLocation.PRIVATE}`}
+              >
+                Private
+              </Link>
+            </Button>
+          </div>
+          <div className="text-center">
+            <img
+              width={600}
+              src={process.env.PUBLIC_URL + "/img/others/img-18.png"}
+              alt="Cloud"
+            />
+          </div>
+        </Route>
+        <Route exact path={`${props.match.url}/instance`}>
+          <Instance {...props} />
+        </Route>
+        <Route path={`${props.match.url}/instance/create`}>
+          <InstanceCreate {...props} />
+        </Route>
+        <Route path={`${props.match.url}/instance/edit`}>
+          <InstanceEdit {...props} />
         </Route>
       </Switch>
     </>
