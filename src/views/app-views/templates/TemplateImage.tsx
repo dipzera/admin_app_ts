ff3905b6ac5000d36c12e674a3553f13367691be
dipzera <nathanielthomaz@gmail.com>
2021-04-08 17:45:54 +0300

    Added loupe when hovering a template image

--- src/views/app-views/templates/TemplateImage.tsx
+++ src/views/app-views/templates/TemplateImage.tsx
@@ -1,18 +1,23 @@
 import * as React from "react";
 import { Avatar } from "antd";
 import { ExperimentOutlined } from "@ant-design/icons";
+import "./template.scss";
 interface TemplateImageProps {
   image: string;
-  setPreview: React.SetStateAction<React.Dispatch<boolean>>;
+  setPreview: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const TemplateImage = ({ image, setPreview }: TemplateImageProps) => {
   return (
-    <div
-      onMouseOver={() => {
-        /* show magnifier */
-      }}
-    >
+    <div className="template-image" onClick={() => setPreview(true)}>
+      <div className="loupe">
+        <img
+          className="loupe__img"
+          src={`${process.env.PUBLIC_URL}/img/loupe.svg`}
+          alt="Loupe"
+        />
+      </div>
       <Avatar
+        className="template-image"
         icon={<ExperimentOutlined />}
         shape="square"
         src={image}
